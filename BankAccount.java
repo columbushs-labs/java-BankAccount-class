9f9767c3aed2374d53439ad2e42ebac83d7e538c
Shariq Shaikh <62669036+shariq-hub@users.noreply.github.com>
2021-10-02 20:50:34 +0500

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,9 +1,33 @@
 public class BankAccount {
-
-    // Instance Variables
-
-    // Constructors
-
-    // Methods
-
+    private String name;
+    private String email;
+    private String phonenumber;
+    private long balance;
+    
+    BankAccount(String name,String email,String phonenumber){
+        this.name=name;
+        this.email=email;
+        this.phonenumber=phonenumber;
+    }
+    
+    public void addbalance(long Balance){
+        if(balance>0){
+        balance+=Balance;
+        }
+        else{
+        System.out.println("Sorry you entered wrong Number or negative Number");
+        }
+    }
+    
+    public void removebalance(long money){
+    if(money>=0){
+    balance-=money;
+    }
+        else{
+        System.out.println("Sorry you entered wrong number");
+         
+        }
+    
+    }
+    
 }
